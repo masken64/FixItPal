bd83457890b30d3344dc068dd68989c6fde6952a
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-11-02 23:31:40 +0530

    initial

---
+++ fixitpal-backend-apis/src/test/java/com/masken/fixitpal/FixitpalBackendApisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masken.fixitpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FixitpalBackendApisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
