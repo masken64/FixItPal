a57c25d5bb836c799ec088c8649f01d04d5cf011
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-11-04 21:03:37 +0530

    Update UserDTO.java

--- fixitpal_backend_api/src/main/java/com/masken/fixitpal_backend_api/payloads/UserDTO.java
+++ fixitpal_backend_api/src/main/java/com/masken/fixitpal_backend_api/payloads/UserDTO.java
@@ -1,7 +1,13 @@
 package com.masken.fixitpal_backend_api.payloads;
 
 import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@NoArgsConstructor
+@Getter
+@Setter
 public class UserDTO {
     private int user_id;
     private String name;
