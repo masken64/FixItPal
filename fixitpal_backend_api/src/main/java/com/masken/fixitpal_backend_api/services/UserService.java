fad3a33e5f59b2d55a833bfbdda39c96b3ff21d0
Kartik Sharma <66724218+masken64@users.noreply.github.com>
2023-11-07 18:12:06 +0530

    service implementation

--- fixitpal_backend_api/src/main/java/com/masken/fixitpal_backend_api/services/UserService.java
+++ fixitpal_backend_api/src/main/java/com/masken/fixitpal_backend_api/services/UserService.java
@@ -1,8 +1,15 @@
 package com.masken.fixitpal_backend_api.services;
 
 import com.masken.fixitpal_backend_api.models.User;
+import com.masken.fixitpal_backend_api.payloads.UserDTO;
+
+import java.util.List;
 
 public interface UserService {
 
-    User createUser(User user);
+    UserDTO createUser(UserDTO user);
+    UserDTO updateUser(UserDTO user, Integer user_id);
+    UserDTO getUser(UserDTO user_id);
+    List<UserDTO> getAllUsers();
+    void deleteUser(Integer user_id);
 }
---
+++ fixitpal_backend_api/src/main/java/com/masken/fixitpal_backend_api/services/impl/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package com.masken.fixitpal_backend_api.services.impl;
+import com.masken.fixitpal_backend_api.payloads.UserDTO;
+import com.masken.fixitpal_backend_api.services.UserService;
+
+import java.util.List;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public UserDTO createUser(UserDTO user){
+        return null;
+    }
+
+    @Override
+    public UserDTO updateUser(UserDTO user, Integer user_id){
+        return null;
+    }
+
+    @Override
+    public UserDTO getUser(UserDTO user_id){
+        return null;
+    }
+
+    @Override
+    public List<UserDTO> getAllUsers(){
+        return null;
+    }
+
+    @Override
+    public  void deleteUser(Integer user_id){
+        return null;
+    }
+}
